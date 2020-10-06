13663d39b2f79011a975698c33cbdcc402499d2c
hongge199 <1259131259@qq.com>
2020-10-06 09:43:34 +0800

    based on the page

---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title','帮助')
+@section('content')
+    <h1>help new pages</h1>
+@endsection
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.default')
+@section('content')
+    <h1>主页!这里采取的是继存方式来的哦</h1>
+@endsection
